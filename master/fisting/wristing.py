71f8740cdcb42f652e38927c3b57d41c7638ec49
SAMBO <sambo522@icloud.com>
2023-02-16 18:02:20 +0700

    16.02.23

---
+++ master/fisting/wristing.py
@@ -0,0 +1,33 @@
+#import random
+#number=random.randint(4, 23)
+#print (number)
+
+#l = [1, 2, 3]
+from time import sleep, time
+from random import randint
+
+#
+# print(3)
+# sleep(0.99)
+# print(2)
+# sleep(1.01)
+# # print(1)
+# # fastshoot
+# t=randint(1,5)
+# sleep(t)
+# start = time() #check time
+# input("shoot!") # time will tick while enter not pressed
+# end = time()
+# dif = end - start
+# print(dif)
+# if dif == 0.0:
+#     print("фльшстарт")
+# elif dif <= 0.6:
+#     print("great")
+# else:
+#     print("SimenCUMF")
+x = 5
+while x > 0:
+    print("Hyinuao")
+    # x = x-1
+while True: #бесконечный цикл
