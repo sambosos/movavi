c04dc4317fccd5d10a49b0f181f9ade21d3846e3
SAMBO <sambo522@icloud.com>
2023-03-02 17:58:41 +0700

    7t42f0k

--- master/fisting/wristing.py
+++ master/fisting/wristing.py
@@ -26,8 +26,88 @@ from random import randint
 #     print("great")
 # else:
 #     print("SimenCUMF")
-x = 5
-while x > 0:
-    print("Hyinuao")
-    # x = x-1
-while True: #бесконечный цикл
+#x = 5
+#while x > 0:
+ #   print("Hyinuao")
+#    # x = x-1
+# #while True: #бесконечный цикл#
+# weather = "пасмурно"
+# day = 1
+# while weather == "пасмурно"
+#     if day == 31 :
+#         weather = "солнечно"
+#         print('улыбнулся')
+#     else:
+#         print("мне грустно")
+#         day = day + 1
+# s = "это я"
+# for pelmeni in s :
+#     print(pelmeni)
+#
+# for vareniki in range (5,15+ 1):
+#     print(vareniki)
+# for vareniki in range(0 , 14, -1):
+#     print(vareniki)
+# l =[ 3, 7, 1, True:, 4.2,'ы']
+# #     print(l[5])
+# t= [True, 2.0, "five", 'ie', [1,2]]
+# # for vareniki in range(len(t)):
+# #     print(t[vareniki])
+# for  ind , arbuz in enumerate(t):
+#     print(f'indtcs:{ind}. znachenie: {arbuz}')
+
+# for i in range(1,11):
+#     if i == 4:
+#         continue # пропуск итерации
+# elif i == 5:
+#     break
+# #     print('prizok №', i )
+# i = []# пустой список
+# i.append('dark1')
+# i.append('dark2')
+# i.append('dark3')
+# x = i.remove('dark2') #удалить по значениям
+# print(x) # .remove() ничего не возвращает
+# #---------------------------------------------
+# u = i.pop(0)# удалить по индексу
+# print(u) # .pop() возвращает значение удаленнного элимента
+# print(i)
+
+# i = ['k', 'n', 'l']
+# print(i.index('n'))
+# i.insert(1,'ANTON')
+# print(i)
+t = (1, 2, 3) # tuple -> неизменяемый
+empty_t =()
+one_t =  ("g,")
+print(one_t)
+print(type(one_t))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
