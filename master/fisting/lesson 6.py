6c8376fb3a507356564e67290cb42f009f591c18
SAMBO <sambo522@icloud.com>
2023-03-16 17:51:02 +0700

    7[]

---
+++ master/fisting/lesson 6.py
@@ -0,0 +1,50 @@
+# mn = {1,2,3,2} # повторение ни-ни
+# slov = {'key': [1,2,3]} # ключ:значение
+
+
+# #функция
+# new*
+# def Ee(): # обьявление функции
+#     print('ы')
+#
+# print(ee())
+# def dd(num1, num2): # с аргументами
+#     ''''''
+#     print(num1+ num2) #возврат
+# dd(5, 1) # вызов с аргументами
+#
+# data(1,4,5,6,7)
+# def f(n):
+#     if n == sorted(data):
+#         print('sorted')
+#         return
+#     else:
+#         print('no sorted')
+# print(i(data))
+
+# l = [1,5,2]
+# print(sorted(l, reverse=True)) # сортировка перевернуть
+# def add (x,y):
+#     return x+y
+#
+# # lambda x,y:x+y
+# def add(x,y):
+#     return x+y
+# lfunc = lambda x, y: x+y
+#
+# print(lfunc(5,7))
+# # if-else в одну строку
+# answer = input('pivo?').upper()
+# print(True if answer == "g" else False)
+variants = ("G",'H',"K")
+result = lambda answer: True if answer in variants else False
+print(result(answer=input("pivo?").upper()))
+
+
+
+
+
+
+
+
+
